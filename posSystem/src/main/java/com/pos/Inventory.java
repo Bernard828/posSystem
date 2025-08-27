bb0556e98cd284665c06eda7b4b6fea00022f322
Bernard828 <bernard.lackey1@gmail.com>
2025-08-27 19:10:01 -0400

    Inventory class used to print available products.

--- posSystem/src/main/java/com/pos/Inventory.java
+++ posSystem/src/main/java/com/pos/Inventory.java
@@ -5,4 +5,28 @@ import java.util.List;
 
 public class Inventory {
     private List<Product> products;
+
+    public Inventor(){
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public Product getProductById(String id) {
+        for(Product product : products){
+            if(product.getId().equals(id)){
+                return product;
+            }
+        }
+        return null; //not found
+    }
+
+    public void listProducts(){
+        System.out.println("Available Products:");
+        for(Product product: products){
+            System.out.println(product);
+        }
+    }
 }
